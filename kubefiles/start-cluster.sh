163a9c0cc0f0224bbafb477649b848b3d822fc32
Jaehyun Lee <buryeye7@naver.com>
2020-05-12 06:44:51 +0000

    a

--- kubefiles/start-cluster.sh
+++ kubefiles/start-cluster.sh
@@ -4,4 +4,6 @@ COUCHDB="http://admin:admin@13.125.228.37:5984"
 
 curl -X PUT $COUCHDB/wallet-address
 kubectl create -f friday-namespace.yaml
-kubectl create configmap node-config --from-file=node-config -n=fct
+kubectl config set-context --current --namespace=my-namespace
+kubectl config get-contexts
+kubectl create configmap node-config --from-file=node-config
--- scripts/start-seed.sh
+++ scripts/start-seed.sh
@@ -21,10 +21,6 @@ done
 # run execution engine grpc server
 $SRC/CasperLabs/execution-engine/target/release/casperlabs-engine-grpc-server -t 8 $HOME/.casperlabs/.casper-node.sock&
 
-mkdir -p $HOME/node-config
-cp -rf $HOME/node-config/.nodef $HOME/
-cp -rf $HOME/node-config/.clif $HOME/
-
 WALLET_ADDRESS=$(clif keys show node1 -a)
 NODE_PUB_KEY=$(nodef tendermint show-validator)
 NODE_ID=$(nodef tendermint show-node-id)
