e7c5974caede04e0b138482d04b5ba0e645c8498
Jaehyun Lee <buryeye7@naver.com>
2020-05-12 11:52:03 +0000

    a

--- scripts/start-seed.sh
+++ scripts/start-seed.sh
@@ -25,9 +25,8 @@ $SRC/CasperLabs/execution-engine/target/release/casperlabs-engine-grpc-server -t
 WALLET_ADDRESS=$(clif keys show node1 -a)
 NODE_PUB_KEY=$(nodef tendermint show-validator)
 NODE_ID=$(nodef tendermint show-node-id)
-IP_ADDRESS=$(hostname -I)
-IP_ADDRESS=$(echo "${IP_ADDRESS}" | sed -e 's/^[[:space:]]*//')
+IP_ADDRESS=$(hostname -I | sed -e 's/^[[:space:]]*//')
 curl -X PUT $COUCHDB/wallet-address/$WALLET_ADDRESS -d "{\"type\":\"seed-node\",\"node-pub-key\":\"$NODE_PUB_KEY\",\"node-id\":\"$NODE_ID\",\"ip-address\":\"$IP_ADDRESS\"}"
-curl -X PUT $COUCHDB/seed-info/seed-info -d "{\"target\":\"$NODE_ID@$IP_ADDRESS:26656\"}"
+curl -X PUT $COUCHDB/seed-info/seed-info -d "{\"target\":\"${NODE_ID}@${IP_ADDRESS}:26656\"}"
 
 nodef start > nodef.txt
